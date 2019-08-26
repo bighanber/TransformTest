c6e62c2a71a63e8dc63c67ea952c4393f81d789f
uuluu <358906125@qq.com>
2019-08-26 20:47:22 +0800

    添加新的Activity

--- app/src/main/java/com/lll/transformtest/MainActivity.kt
+++ app/src/main/java/com/lll/transformtest/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.lll.transformtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.testmodule.FuncConst
+import com.example.testmodule.TestActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         testFun()
+        testFun2()
+        tv_click.setOnClickListener {
+            startActivity(Intent(this, TestActivity::class.java))
+        }
     }
 
     @FuncConst("testFun")
     fun testFun() {
         Thread.sleep(3000)
 //        Log.e("asd", System.currentTimeMillis().toString())
+    }
+
+    @FuncConst("testFun2")
+    fun testFun2() {
+
     }
 }
---
+++ testmodule/src/main/java/com/example/testmodule/TestActivity.kt
@@ -0,0 +1,18 @@
+package com.example.testmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+        newFunc()
+    }
+
+    @FuncConst("newFunc")
+    fun newFunc() {
+
+    }
+}
